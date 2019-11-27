80a2b62e2b4bedf1e64de8bc55ea024e08479ece
ULISES ORTEGA <ulises.ortega@gmail.com>
2019-11-27 10:55:04 -0600

    subida de todo el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,29 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {HttpClientModule} from "@angular/common/http";
+import { DatosnPage } from '../pages/datosn/datosn';
+import { MuestraPage } from '../pages/muestra/muestra';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    DatosnPage,
+    MuestraPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    DatosnPage,
+    MuestraPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { DatosnPage} from "../datosn/datosn";
+import { MuestraPage} from "../muestra/muestra";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  contactos:any;
+  DatosnPage = DatosnPage;
+  MuestraPage = MuestraPage;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,
+    public http: HttpClient) {
+      //this.http.get('/proyecto/proyecto/')
+      //.subscribe(data=>{
+        //this.contactos=data;
+        //console.log(JSON.stringify(data));
+      //},error => {
+        //console.log(JSON.stringify(error));
+      //});
   }
+  ionViewWillEnter(){
+    this.http.get('/proyecto/proyecto/')
+      .subscribe(data=>{
+        this.contactos=data;
+        console.log(JSON.stringify(data));
+      },error => {
+        console.log(JSON.stringify(error));
+      });
+  }
+
+  clickAdd(){
+    console.log('Si QUEDO');
+    this.navCtrl.push(this.DatosnPage);
+  }
+
+  clickFoto(img,nombre,telefono,correo,facebook,twitter,instagram){
+    this.navCtrl.push(this.MuestraPage,{img:img, nombre:nombre, telefono:telefono, correo:correo, facebook:facebook, twitter:twitter, instagram:instagram});
+  }
+
 
 }
